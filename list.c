1abab030dd7e8ac2dbe59e2d737d678e7faca8d5
KenLi8504 <kli22@stuy.edu>
2021-10-18 03:16:02 -0400

    Fixed a seg fault error, added in the free_list function

--- list.c
+++ list.c
@@ -11,10 +11,13 @@ struct student{
 };
 
 void print_list (struct student * stu){
-  printf("This student got %d hours of sleep on average.\n",stu -> sleep);
-  printf("This student's gpa is %lf.\n",stu -> gpa);
-  printf("This student is feeling %s.\n",stu -> mood);
-  print_list(stu -> next);
+  if (stu == NULL){
+    return;
+  }
+    printf("This student got %d hours of sleep on average.\n",stu -> sleep);
+    printf("This student's gpa is %lf.\n",stu -> gpa);
+    printf("This student is feeling %s.\n",stu -> mood);
+    print_list(stu -> next);
 }
 
 struct student * insert_front(struct student * firstStu,int hours, double grade, char mentalState []){
@@ -30,14 +33,30 @@ struct student * makeStudent(int hours,double grade, char mentalState[]){
   struct student *stu = malloc(sizeof(struct student));
   stu -> sleep = hours;
   stu -> gpa = grade;
+  stu -> next = NULL;
   strcpy(stu -> mood,mentalState);
   return stu;
 }
 
+struct student *free_list(struct student * lastStu){
+  while (lastStu != NULL){
+    struct student *placeholder = lastStu -> next;
+    free(lastStu);
+    lastStu = placeholder;
+  }
+  return lastStu;
+}
+
 int main(){
-  struct student * example2 = insert_front(NULL,5,90,"Sad");
+  printf("This is the list of students \n");
+  struct student * example1 = makeStudent(10,99,"Happy");
+  struct student * example2 = insert_front(example1,5,90,"Sad");
   struct student * example3 = insert_front(example2,3,100,"Exhausted");
   struct student * example4 = insert_front(example3,0,100,"No emotions");
   struct student * example5 = insert_front(example4,8,80,"Depressed");
   print_list(example5);
+
+  printf("\nThis is the list, now all released\n");
+  struct student * example6 = free_list(example5);
+  print_list(example6);
 }
